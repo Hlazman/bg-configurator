2453b1c016f9223da940c1685c7e0ccf01905046
Hlazman <glazman.b@gmail.com>
2023-07-08 18:16:18 +0300

    made Auth page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, ConfigProvider } from 'antd';
+import { Route, Routes } from 'react-router-dom';
+import { NotFoundPage } from './Pages/NotFoundPage';
+import { AuthPage } from './Pages/AuthPage';
+import { TempPage } from './Pages/TempPage';
+
 
 function App() {
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<TempPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </div>
+  
   );
 }
 
---
+++ src/Pages/AuthPage.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Card, Form, Input, Checkbox, Button, Typography } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+import logo from '../logo.svg';
+
+const { Title } = Typography;
+
+export const AuthPage = () => {
+  const onFinish = (values) => {
+    console.log('Received values:', values);
+  };
+
+  return (
+    <>
+      <img src={logo} alt="Logo" style={{ height: 120, margin: 20 }} />
+
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+        <Card style={{ width: 400 }}>
+          <div style={{ textAlign: 'left' }}>
+            <Title level={4} style={{ margin: '20px 0', color: '#343339', textAlign: 'center' }}>Sign in to your account</Title>
+          </div>
+
+          <Form name="login-form" onFinish={onFinish}>
+
+            <Form.Item
+              name="email"
+              rules={[
+                { required: true, message: 'Please input your email!' },
+                { type: 'email', message: 'Please enter a valid email address!' },
+              ]}
+            >
+              <Input prefix={<UserOutlined />} placeholder="Email" type="email" style={{ height: 48 }} />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password prefix={<LockOutlined />} placeholder="Password" style={{ height: 48 }} />
+            </Form.Item>
+
+            <Form.Item name="remember" valuePropName="checked" style={{ marginBottom: 20, textAlign: 'left', colorPrimary: '#f06d20'}}>
+              <Checkbox style={{ color: '#343339' }}>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item style={{ marginBottom: 20 }}>
+              <Button type="primary" htmlType="submit" style={{ width: '100%', backgroundColor: '#f06d20', height: 48 }}>Sign in</Button>
+            </Form.Item>
+
+            <Form.Item style={{ marginBottom: 12 }}>
+              <a href="#" style={{ color: '#f06d20' }}>Forgot password?</a>
+            </Form.Item>
+
+            <Form.Item>
+              <span style={{ color: '#343339' }}>Don’t have an account? </span>
+              <a href="#" style={{ color: '#f06d20' }}>Sign up</a>
+            </Form.Item>
+
+          </Form>
+
+      </Card>
+    </div>
+    </>
+  );
+};
