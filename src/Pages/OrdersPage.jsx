2c0102f3d624965fca967ff7fca0398567077fa4
Hlazman <glazman.b@gmail.com>
2023-07-11 18:21:10 +0300

    add settings to table

--- src/Pages/OrdersPage.jsx
+++ src/Pages/OrdersPage.jsx
@@ -1,82 +1,108 @@
-import { Table } from 'antd';
+import { Table, Dropdown, Space } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 import { useState } from 'react';
 
+const items = [
+  {
+    key: '1',
+    label: 'Action 1',
+  },
+  {
+    key: '2',
+    label: 'Action 2',
+  },
+];
+
 const columns = [
+  // Table.SELECTION_COLUMN,
+  // Table.EXPAND_COLUMN,
   {
     title: 'Full Name',
-    width: 100,
     dataIndex: 'name',
     key: 'name',
     fixed: 'left',
+    sorter: (a, b) => a.age - b.age,
   },
   {
     title: 'Age',
     width: 100,
     dataIndex: 'age',
     key: 'age',
+    sorter: (a, b) => a.age - b.age,
   },
   {
     title: 'Column 1',
     dataIndex: 'address',
     key: '1',
-    width: 150,
+    sorter: (a, b) => a.age - b.age,
   },
   {
     title: 'Column 2',
     dataIndex: 'address',
     key: '2',
-    width: 150,
+    sorter: (a, b) => a.age - b.age,
   },
   {
     title: 'Column 3',
     dataIndex: 'address',
     key: '3',
-    width: 150,
+    sorter: (a, b) => a.age - b.age,
   },
   {
     title: 'Column 4',
     dataIndex: 'address',
     key: '4',
-    width: 150,
+    sorter: (a, b) => a.age - b.age,
   },
   {
     title: 'Column 5',
     dataIndex: 'address',
     key: '5',
-    width: 150,
+    sorter: (a, b) => a.age - b.age,
   },
   {
     title: 'Column 6',
     dataIndex: 'address',
     key: '6',
-    width: 150,
+    sorter: (a, b) => a.age - b.age,
   },
   {
     title: 'Column 7',
     dataIndex: 'address',
     key: '7',
-    width: 150,
+    sorter: (a, b) => a.age - b.age,
   },
   {
     title: 'Column 8',
     dataIndex: 'address',
     key: '8',
+    sorter: (a, b) => a.age - b.age,
   },
+
   {
     title: 'Action',
+    dataIndex: 'operation',
     key: 'operation',
     fixed: 'right',
-    width: 100,
-    render: () => <a>action</a>,
+    with: '80px',
+    render: () => (
+      <Space size="large">
+        <Dropdown menu={{items}}>
+          <p>Actions <DownOutlined /> </p>
+        </Dropdown>
+      </Space>
+    ),
   },
 ];
+
 const data = [];
 for (let i = 0; i < 100; i++) {
   data.push({
     key: i,
     name: `Edward ${i}`,
-    age: 32,
+    age: 32+i,
     address: `London Park no. ${i}`,
+    description: "This not expandable"
   });
 }
 
@@ -92,7 +118,12 @@ export const OrdersPage = () => {
 
   return (
     <Table
-      style={{marginTop: '20px'}}
+      rowSelection={{}}
+      expandable={{
+        expandedRowRender: record => (
+          <p style={{ margin: 0 }}>{record.description}</p>
+        ),
+      }}
       columns={columns}
       dataSource={data}
       scroll={{
