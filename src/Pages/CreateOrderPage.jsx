f0d03c568461f8eaaecea07794e838f892734cf6
Hlazman <glazman.b@gmail.com>
2023-07-22 13:43:44 +0300

    made radio image button and searchbar in create order page

--- src/Pages/CreateOrderPage.jsx
+++ src/Pages/CreateOrderPage.jsx
@@ -1,11 +1,25 @@
 import React, { useState } from 'react';
-import { Steps, Form, Input, Button } from 'antd';
+import { Steps, Form, Input, Button, Radio, Card } from 'antd';
 import { OrderDetails } from '../Components/OrderDetails';
+
+import im1 from '../tempData/im1.webp';
+import im2 from '../tempData/im2.webp';
+import im3 from '../tempData/im3.webp';
+import im4 from '../tempData/im4.webp';
+import im5 from '../tempData/im5.webp';
+import im6 from '../tempData/im6.webp';
+
+const imgS = [im1, im2, im3, im4, im5, im6];
 
 export const CreateOrderPage = () => {
 
   const [currentStep, setCurrentStep] = useState(0);
   const [formData, setFormData] = useState({});
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearchInputChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
 
   const handleFormSubmit = (values) => {
     setFormData({ ...formData, ...values });
@@ -28,13 +42,55 @@ export const CreateOrderPage = () => {
     setFormData((prevFormData) => ({ ...prevFormData, ...changedValues }));
   };
 
+  const handleCardClick = (value) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      step1Field: prevFormData.step1Field === value ? null : value
+    }));
+  };
+
+
+  const filteredImgS = imgS.filter((imgSrc, index) =>
+    `Image ${index + 1}`.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   const renderFormStep = () => {
     switch (currentStep) {
       case 0:
         return (
           <Form onFinish={handleFormSubmit} onValuesChange={handleFormValuesChange}>
+            <p>Choose something from this form</p>
+             <Input
+              placeholder="Search by title"
+              value={searchQuery}
+              onChange={handleSearchInputChange}
+              style={{ marginBottom: '10px' }}
+            />
             <Form.Item name="step1Field" label="Шаг 1">
-              <Input />
+              <Radio.Group value={formData.step1Field}>
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                  {filteredImgS.map((imgSrc, index) => (
+                    <div key={index} style={{ width: 200, margin: '10px' }}>
+                      <Card
+                        hoverable
+                        style={{ border: formData.step1Field === `image${index + 1}.webp` ? '7px solid #f06d20' : 'none' }}
+                        onClick={() => handleCardClick(`image${index + 1}.webp`)}
+                      >
+                        <div style={{ overflow: 'hidden', height: 220 }}>
+                          <img
+                            src={imgSrc}
+                            alt={`${index + 1}`}
+                            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                            // onClick={() => handleCardClick(`image${index + 1}.webp`)}
+                          />
+                        </div>
+                        <Card.Meta title={`Image ${index + 1}`} style={{ paddingTop: '10px' }} />
+                        <Radio value={`image${index + 1}.webp`} style={{ display: 'none' }} />
+                      </Card>
+                    </div>
+                  ))}
+                </div>
+              </Radio.Group>
             </Form.Item>
             <Button type="primary" onClick={handleNext}> Далее </Button>
           </Form>
